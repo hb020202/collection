28cd9bdabe27790c449e319850f6d6fd74ce67c4
Holly Bedford <holly.bedford02@gmail.com>
2020-09-29 12:03:09 +0100

    connection separated

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
-<?php
-$db = new PDO ('mysql:host=db; dbname=collection', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare("SELECT * FROM `books`;");
-$query->execute();
-?>
+
+<?php require('dbconn.php'); ?>
 <!doctype html>
 <html lang="en">
 <head>
